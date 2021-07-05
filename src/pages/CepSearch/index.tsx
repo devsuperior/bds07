b8d228c3e7d71e961ab69e79aca5469a42c1975e
Nelio Alves <acenelio@gmail.com>
2021-07-05 08:58:07 -0300

    Form events

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,30 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import React from 'react';
 
 const CepSearch = () => {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("Mudou para: " + event.target.value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Clicou");
+  }
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
