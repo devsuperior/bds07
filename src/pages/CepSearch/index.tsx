e763e2292f2ad80cbcab0a5f257e2649fa054754
Nelio Alves <acenelio@gmail.com>
2021-07-05 09:17:33 -0300

    Form structure

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -2,17 +2,32 @@ import './styles.css';
 
 import ResultCard from 'components/ResultCard';
 import React from 'react';
+import { useState } from 'react';
+
+type FormData = {
+  cep: string;
+  test: string;
+};
 
 const CepSearch = () => {
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+    test: '',
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Mudou para: " + event.target.value);
-  }
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log("Mudou: " + name + " para valor " + value);
+
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Clicou");
-  }
+    console.log(formData);
+  };
 
   return (
     <div className="cep-search-container">
@@ -22,8 +37,18 @@ const CepSearch = () => {
           <div className="form-container">
             <input
               type="text"
+              name="cep"
               className="search-input"
               placeholder="CEP (somente números)"
+              value={formData.cep}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="test"
+              className="search-input"
+              placeholder="Test"
+              value={formData.test}
               onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
@@ -34,7 +59,6 @@ const CepSearch = () => {
 
         <ResultCard title="Logradouro" description="Lalala" />
         <ResultCard title="Número" description="234" />
-
       </div>
     </div>
   );
